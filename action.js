6b6f76a45e18bbca796aec2d057c1c21dc9fd94b
Abhishek Tiwar <tiwari.abhishek428@gmail.com>
2017-01-17 23:55:25 +0530

    Changes in Product page

--- action.js
+++ action.js
@@ -11,7 +11,20 @@ document.getElementById('head_name').innerHTML='Industrial Display';
 
 else if(x=='industrial_clock') {
 document.getElementById('head_name').innerHTML='Industrial Clock';
-document.getElementById('r1').innerHTML='Micro-controller based design with or without GPS connectivity.'+'<br>'+'<br><span>For GPS clock</span>: Automatic time setting from GPS satellite.'+'<br>'+'<br><span>Options</span>: Double side clock or master/slave clock'+'<br>'+'<br><span>Clock format</span>: 12 Hrs. or 24 Hrs. (HH:MM:SECS or HH:MM)'+'<br>'+'Inbuilt battery backup';
+document.getElementById('r3d1').innerHTML ='<span>For GPS clock:</span>';
+document.getElementById('r3d2').innerHTML ='Automatic time setting from GPS satellite.';
+document.getElementById('r4d1').innerHTML ='<span>Options:</span>';
+document.getElementById('r4d2').innerHTML =' Double side clock or master/slave clock';
+document.getElementById('r5d1').innerHTML ='<span>Clock format:</span>';
+document.getElementById('r5d2').innerHTML ='12 Hrs. or 24 Hrs. (HH:MM:SECS or HH:MM)';
+document.getElementById('r1d1').innerHTML ='';
+document.getElementById('r1d2').innerHTML ='Micro-controller based design with or without GPS connectivity';
+document.getElementById('r2d1').innerHTML ='';
+document.getElementById('r2d2').innerHTML ='Inbuilt battery backup';
+document.getElementById('r6d1').innerHTML ='';
+document.getElementById('r6d2').innerHTML ='';
+document.getElementById('r7d1').innerHTML ='';
+document.getElementById('r7d2').innerHTML ='';
 }
 
 else if(x=='lfm')
@@ -22,12 +35,38 @@ document.getElementById('head_name').innerHTML='Megawatt Pannel';
 
 else if(x=='process_n_indicator'){
 document.getElementById('head_name').innerHTML='Process and Indicator';
-document.getElementById('r1').innerHTML ='Used for the measurement of process parameters'+'<br>'+'Input: 4 to 20 milli amp.'+'<br>'+'Range can be set through push buttons from 0-9999 & zero setting is automatic.'+'<br>'+'4 digit LED display'+'<br>' +'<br><span>Display digit</span>: 7 segment red LED display'+'<br>'+'<br><span>Digit height</span>: 12.5mm, 25mm, 50mm, 100mm, 150mm, 200mm,  300mm'+'<br>'+'Decimal placement is settable';
+document.getElementById('r5d1').innerHTML ='<span>Input:</span>';
+document.getElementById('r5d2').innerHTML ='4 to 20 milli amp';
+document.getElementById('r6d1').innerHTML ='<span>Display digit:</span>';
+document.getElementById('r6d2').innerHTML ='7 segment red LED display';
+document.getElementById('r7d1').innerHTML ='<span>Digit height:</span>';
+document.getElementById('r7d2').innerHTML ='12.5mm, 25mm, 50mm, 100mm, 150mm, 200mm,  300mm';
+document.getElementById('r1d1').innerHTML ='';
+document.getElementById('r1d2').innerHTML ='Used for the measurement of process parameters';
+document.getElementById('r2d1').innerHTML ='';
+document.getElementById('r2d2').innerHTML ='Range can be set through push buttons from 0-9999 & zero setting is automatic';
+document.getElementById('r3d1').innerHTML ='';
+document.getElementById('r3d2').innerHTML ='4 digit LED display';
+document.getElementById('r4d1').innerHTML ='';
+document.getElementById('r4d2').innerHTML ='Decimal placement is settable';
 }
 
 else if(x=='ph_meter') {
 document.getElementById('head_name').innerHTML='Ph Meter';
-document.getElementById('r1').innerHTML ='<span>Range</span>: for pH: 0-14pH '+'<br>'+'for mV: (-1999) mV to (+1999) mV (optional)'+'<br>'+'<br><span>Temperature compensation</span>: 0-100degC (manual)'+'<br>'+'<br><span>Accuracy</span>: ±1 mV ±1count'+'<br>'+'<br><span>Resolution</span>: 0.01 pH;'+'<br>' +' 1mV 3 ½ digit LED display'+'<br>'+'<br><span>Options</span>: Analog recorder output facility'+'<br>'+'<br><span>Accessories</span>: pH electrodes, buffer tablets';
+document.getElementById('r2d1').innerHTML ='<span>Range:</span>';
+document.getElementById('r2d2').innerHTML ='for pH: 0-14pH for mV: (-1999) mV to (+1999) mV (optional)';
+document.getElementById('r3d1').innerHTML ='<span>Temperature compensation:</span>';
+document.getElementById('r3d2').innerHTML =' 0-100degC (manual)';
+document.getElementById('r4d1').innerHTML ='<span>Accuracy:</span>';
+document.getElementById('r4d2').innerHTML ='±1 mV ±1count';
+document.getElementById('r5d1').innerHTML ='<span>Resolution:</span>';
+document.getElementById('r5d2').innerHTML ='0.01 pH; 1mV';
+document.getElementById('r1d1').innerHTML ='';
+document.getElementById('r1d2').innerHTML ='3 ½ digit LED display';
+document.getElementById('r6d1').innerHTML ='<span>Options: </span>';
+document.getElementById('r6d2').innerHTML =' Analog recorder output facility';
+document.getElementById('r7d1').innerHTML ='<span>Accessories:</span>';
+document.getElementById('r7d2').innerHTML ='pH electrodes, buffer tablets';
 }
 
 else if(x=='scrolling_display')
@@ -35,25 +74,74 @@ document.getElementById('head_name').innerHTML='Scrolling Display';
 
 else if(x=='tachometer') {
 document.getElementById('head_name').innerHTML='Tachometer';
-document.getElementById('r1').innerHTML='Non-contact speed sensing.'+'<br>'+'High accuracy ( better than 0.1% ± 1 r.p.m).'+'<br>'+'4 digit LED display.'+'<br>'+'<br><span>Range</span>: 0-9999 rpm'+'<br>'+'Non blinking display'+'<br>' +'<br><span>Transducer</span>: Magnetic pick-up with 5mtrs. Lead'+'<br>'+'<br><span>Operating voltage</span>: 230V ± 15%, 50Hz (standard)';
-
+document.getElementById('r5d1').innerHTML ='<span>Range:</span>';
+document.getElementById('r5d2').innerHTML ='0-9999 rpm';
+document.getElementById('r6d1').innerHTML ='<span>Transducer:</span>';
+document.getElementById('r6d2').innerHTML =' Magnetic pick-up with 5mtrs. Lead';
+document.getElementById('r7d1').innerHTML ='<span>Operating voltage:</span>';
+document.getElementById('r7d2').innerHTML =' 230V ± 15%, 50Hz (standard)';
+document.getElementById('r1d1').innerHTML ='';
+document.getElementById('r1d2').innerHTML ='Non blinking display';
+document.getElementById('r2d1').innerHTML ='';
+document.getElementById('r2d2').innerHTML ='4 digit LED display';
+document.getElementById('r3d1').innerHTML ='';
+document.getElementById('r3d2').innerHTML ='Non-contact speed sensing';
+document.getElementById('r4d1').innerHTML ='';
+document.getElementById('r4d2').innerHTML ='High accuracy ( better than 0.1% ± 1 r.p.m)';
 }
 
 else if(x=='temp_cont') {
 document.getElementById('head_name').innerHTML='Temperature Controller';
-document.getElementById('r1').innerHTML='<span>a.</span>Potentiometer setting is available.'+'<br>'+'<br><span>b.</span>ON/OFF control action at pre-set levels is provided by a set of change-over contacts'+'<br>'+'<br><span>c.</span>Available for single set point or dual set point.';
+document.getElementById('r1d1').innerHTML ='';
+document.getElementById('r1d2').innerHTML ='Potentiometer setting is available';
+document.getElementById('r2d1').innerHTML ='';
+document.getElementById('r2d2').innerHTML ='ON/OFF control action at pre-set levels is provided by a set of change-over contacts';
+document.getElementById('r3d1').innerHTML ='';
+document.getElementById('r3d2').innerHTML ='Available for single set point or dual set point';
+document.getElementById('r4d1').innerHTML ='';
+document.getElementById('r4d2').innerHTML ='';
+document.getElementById('r5d1').innerHTML ='';
+document.getElementById('r5d2').innerHTML ='';
+document.getElementById('r6d1').innerHTML ='';
+document.getElementById('r6d2').innerHTML ='';
+document.getElementById('r7d1').innerHTML ='';
+document.getElementById('r7d2').innerHTML ='';
+
 }
 
 else if(x=='twilight_switches') {
 document.getElementById('head_name').innerHTML='Twilight Switches';
-document.getElementById('r1').innerHTML='Switches ON and OFF at dusk and dawn.'+'<br>'+'<br><span>Contact rating:</span> (5, 10, 16, 25, 32, 63, 100) Amps.'+'<br>'
-+'<br><span>No. of poles</span>:'+'<br>'+'a.	For (5, 10, 25) amps. : 02'+'<br>'+'b. For (10, 16, 32, 63 and 100) amps. : 03'+'<br>'+'Electromagnetic relay / contractor is provided as a switching element'
-+'<br>'+'Miniature circuit breaker and HRC fuse used for over load protection';
+document.getElementById('r4d1').innerHTML ='<span>Contact rating:</span>';
+document.getElementById('r4d2').innerHTML ='(5, 10, 16, 25, 32, 63, 100) Amps.';
+document.getElementById('r5d1').innerHTML ='<span>No. of poles:</span>';
+document.getElementById('r5d2').innerHTML ='<ul><li>For (5, 10, 25) amps. : 02</li><li>For (10, 16, 32, 63 and 100) amps. : 03</li></ul>';
+document.getElementById('r1d1').innerHTML ='';
+document.getElementById('r1d2').innerHTML ='Switches ON and OFF at dusk and dawn.';
+document.getElementById('r2d1').innerHTML ='';
+document.getElementById('r2d2').innerHTML ='Electromagnetic relay / contractor is provided as a switching element';
+document.getElementById('r3d1').innerHTML ='';
+document.getElementById('r3d2').innerHTML ='Miniature circuit breaker and HRC fuse used for over load protection';
+document.getElementById('r6d1').innerHTML ='';
+document.getElementById('r6d2').innerHTML ='';
+document.getElementById('r7d1').innerHTML ='';
+document.getElementById('r7d2').innerHTML ='';
 }
 else if(x=='token') {
 document.getElementById('head_name').innerHTML='Token Display';
-document.getElementById('r1').innerHTML='Token displays are extremely useful in bringing order in the waiting area.'+'<br>'+'Bright and large displays are clearly visible from a good distance.'+'<br>'+'<br><span>Options</span>: With Dot LED or With integrated 7 segment display'+'<br>'+'Numeric keypad unit with 05inch display'+'<br>'+'No. of digit is 03 and color of display is Red';
-
+document.getElementById('r5d1').innerHTML ='<span>Options:</span>';
+document.getElementById('r5d2').innerHTML ='With Dot LED or With integrated 7 segment display';
+document.getElementById('r1d1').innerHTML ='';
+document.getElementById('r1d2').innerHTML ='Token displays are extremely useful in bringing order in the waiting area.';
+document.getElementById('r2d1').innerHTML ='';
+document.getElementById('r2d2').innerHTML ='Bright and large displays are clearly visible from a good distance.';
+document.getElementById('r3d1').innerHTML ='';
+document.getElementById('r3d2').innerHTML ='Numeric keypad unit with 05inch display';
+document.getElementById('r4d1').innerHTML ='';
+document.getElementById('r4d2').innerHTML ='No. of digit is 03 and color of display is Red';
+document.getElementById('r6d1').innerHTML ='';
+document.getElementById('r6d2').innerHTML ='';
+document.getElementById('r7d1').innerHTML ='';
+document.getElementById('r7d2').innerHTML ='';
 }
 
 else if(x=='wsm')
@@ -61,20 +149,20 @@ document.getElementById('head_name').innerHTML='Weighing Scale Monitor';
 
 
 
-document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="480" height="350" src='+x+'.jpg'+' alt="">';
+document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="450" height="335" src='+x+'.jpg'+' alt="">';
 document.getElementById('product_image1').innerHTML='<img style="border-radius:10px;border:2px dashed orange;" src=temp_cont.jpg'+' alt="" class="responsive-img">';
 document.getElementById('product_image2').innerHTML='<img style="border-radius:10px;border:2px dashed orange;" src=lfm.jpg'+' alt="" class="responsive-img">';
 document.getElementById('product_image3').innerHTML='<img style="border-radius:10px;border:2px dashed orange;" src='+x+'.jpg'+' alt="" class="responsive-img">';
 }
 
 function set2(x){
-document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="480" height="350" src=temp_cont.jpg'+' alt="">';
+document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="450" height="335" src=temp_cont.jpg'+' alt="">';
 
 }
 function set3(x){
-document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="480" height="350" src=lfm.jpg'+' alt="">';
+document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="450" height="335" src=lfm.jpg'+' alt="">';
 }
 
 function set4(x){
-document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="480" height="350" src='+x+'.jpg'+' alt="">';	
+document.getElementById('product_image').innerHTML='<img style="border-radius:10px;" width="450" height="335" src='+x+'.jpg'+' alt="">';	
 }
--- products.php
+++ products.php
@@ -25,27 +25,25 @@ $x= $_GET['id'];
     $(document).ready(function(){
     $('.materialboxed').materialbox();
     set1('<?php echo $x ?>');
-     $(".btn1").click(function(){
-        $("#product_image").fadeOut('fast', function() {
-          set2('<?php echo $x ?>');
-          $("#product_image").fadeIn("fast");
-        });;
+    $(".btn1").click(function(){
+    $("#product_image").fadeOut('fast', function() {
+    set2('<?php echo $x ?>');
+    $("#product_image").fadeIn("fast");
+    });;
     });
     
     $(".btn2").click(function(){
-        $("#product_image").fadeOut('fast', function() {
-          set3('<?php echo $x ?>');
-          $("#product_image").fadeIn("fast");
-        });;
+    $("#product_image").fadeOut('fast', function() {
+    set3('<?php echo $x ?>');
+    $("#product_image").fadeIn("fast");
+    });;
     });
-
     $(".btn3").click(function(){
-        $("#product_image").fadeOut('fast', function() {
-          set4('<?php echo $x ?>');
-          $("#product_image").fadeIn("fast");
-        });;
+    $("#product_image").fadeOut('fast', function() {
+    set4('<?php echo $x ?>');
+    $("#product_image").fadeIn("fast");
+    });;
     });
-
     $(".dropdown-button").dropdown({
     hover:true,
     constrain_width: true,
@@ -91,86 +89,105 @@ $x= $_GET['id'];
         </div>
       </nav>
     </div>
-    <!--
-    <div class="slider">
-      <ul class="slides">
-        <li>
-          <img src="banner_1.jpg">
-        </li>
-        <li>
-          <img src="banner_2.jpg">
-        </li>
-        
-        <li>
-          <img src="banner_16.jpg">
-        </li>
-        
-        <li>
-          <img src="banner_6.jpg">
-        </li>
-        <li>
-          <img src="banner_24.jpg">
-        </li>
-        
-        <li>
-          <img src="banner_22.jpg">
-          
-        </li>
-        <li>
-          <img src="banner_21.jpg">
-          
-        </li>
-      </ul>
-    </div>-->
-    <h2 class="headName" id="head_name"></h2>
-    <hr>
+    
     <div class="row">
-      
-      <div class="col l5">
-      <div class="row">
-      <div class="col l12 materialboxed" id="product_image">
+      <div class="col l9 s12 m12">
+        <h2 class="headName" id="head_name" style="/*background:#f5f5f5; */background: linear-gradient(#eeeeee,white );" ></h2>
+        <hr>
         
-      </div>
-      </div>
-      <br><br>
-      <div class="row">
-        <div class="col l3 m3 s3 btn1" id="product_image1"></div>
-        <div class="col l3 m3 s3 btn2" id="product_image2"></div>
-        <div class="col l3 m3 s3 btn3" id="product_image3"></div>
-      </div>
-      </div>
-      <div class="col l4 s11 m11">
-        <h4 class="headNamev2" id="r1">specs</h4>
-      </div>
-      <div class="col l2 offset-l1 m12 s12">
-        <div class="row" style="background: cyan;">Request for quote</div>
-        <form>
-          <div class="input-field col s12">
-            <input id="name" type="text" class="validate">
-            <label for="name">Name</label>
+        <div class="col l5">
+          <div class="row">
+          <br><br><br>
+            <div class="col l12 s12 materialboxed" id="product_image">
+              
+            </div>
           </div>
-          <div class="input-field col s12">
-            <input id="email" type="email" class="validate">
-            <label for="email">Email</label>
+          <br><br>
+          <div class="row">
+            <div class="col l3 offset-l2 m3 offset-m2 s3 offset-s2 btn1" id="product_image1"></div>
+            <div class="col l3 m3 s3 btn2" id="product_image2"></div>
+            <div class="col l3 m3 s3 btn3" id="product_image3"></div>
           </div>
-          <div class="input-field col s12">
-            <input id="company" type="text" class="validate">
-            <label for="company">Company</label>
-          </div>
-          <div class="input-field col s12">
-            <input id="phone" type="text" class="validate">
-            <label for="phone">Phone no.</label>
-          </div>
-          <div class="input-field col s12">
-            <input id="msg" type="text" class="validate">
-            <label for="msg">Message</label>
-          </div>
-          <input type="submit" class="waves-effect waves-light btn-flat hoverable">
-        </form>
+        </div>
+        
+        <div class="col l6 offset-l1 s11 m11">
+          <table style="line-height: 160%;">
+          <center><h3 class="headName">Specifications</h3></center>
+            <tr>
+              <td class="headNamev2" id="r1d1"></td>
+              <td class="headNamev2" id="r1d2"></td>
+            </tr>
+            <tr>
+              <td class="headNamev2" id="r2d1"></td>
+              <td class="headNamev2" id="r2d2"></td>
+            </tr>
+            <tr>
+              <td class="headNamev2" id="r3d1"></td>
+              <td class="headNamev2" id="r3d2"></td>
+            </tr>
+            <tr>
+              <td class="headNamev2" id="r4d1"></td>
+              <td class="headNamev2" id="r4d2"></td>
+            </tr>
+            <tr>
+              <td class="headNamev2" id="r5d1"></td>
+              <td class="headNamev2" id="r5d2"></td>
+            </tr>
+            <tr>
+              <td class="headNamev2" id="r6d1"></td>
+              <td class="headNamev2" id="r6d2"></td>
+            </tr>
+            <tr>
+              <td class="headNamev2" id="r7d1"></td>
+              <td class="headNamev2" id="r7d2"></td>
+            </tr>
+          </table>
+          
+        </div>
       </div>
+      <div class="col l2 offset-l1 m12 s12" id="request_for_quote">
+      <legend class="white-text" style="position:relative; background: lightblue;height: 40px;width: 100%;"><br><center>Contact Us</center></legend>
+          
+      <img src="contact.jpg">
+        <ul>
+        <li>+91 9415085804</li>
+        <li>+91-9415039393</li>
+        <li>info@electromed.co.in</li>         
+       </ul>
+        <fieldset>
+          <legend class="white-text" style="position:relative; background: lightblue;height: 40px;width: 100%;"><br><center>Request for quote</center></legend>
+          <form>
+           <input id="name" type="text" class="validate" placeholder="Name" style="font-size:15px;height:25px;">
+            <input id="email" type="email" class="validate" placeholder="Email" style="font-size:15px;height:25px;">
+            <input id="company" type="text" class="validate" placeholder="Company" style="font-size:15px;height:25px;">
+            <input id="phone" type="text" class="validate" placeholder="Phone" style="font-size:15px;height:25px;">
+            <textarea name="" placeholder="Message" style="font-size:15px;"></textarea>
+            <input type="submit" class="btn blue lighten-2">
+         </form>
+        </fieldset>
+      </div>
+      
     </div>
 
-    
+    <div>
+    <br><br>
+      <h2 class="headName">Related Products</h2>
+      <hr>
+      <br>
+      <div class="row" id="related_products">
+        
+        <div class="col l2 offset-l1"><img style="border-radius:10px;" src='temp_cont.jpg'+' alt="" class="responsive-img">
+        <a>know more</a></div>
+        <div class="col l2"><img style="border-radius:10px;" src='temp_cont.jpg'+' alt="" class="responsive-img">
+        <a>know more</a></div>
+        <div class="col l2"><img style="border-radius:10px;" src='temp_cont.jpg'+' alt="" class="responsive-img">
+        <a>know more</a></div>
+        <div class="col l2"><img style="border-radius:10px;" src='temp_cont.jpg'+' alt="" class="responsive-img">
+        <a>know more</a></div>
+
+      </div>
+    </div>
+    <br><br>
     <footer class="page-footer grey">
       <div class="container">
         <div class="row">
